46d0cdf56239f3aa734f38c51d3d63dc972d56f3
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-04-01 18:06:00 +0530

    Typealias pushed

---
+++ src/main/kotlin/variableTypes/TypeAlias.kt
@@ -0,0 +1,11 @@
+package variableTypes
+
+import declarations.Employee
+typealias EmployeeSet = Set<Employee>
+fun main() {
+    val number : Int
+}
+class TypeAlias {
+    val root = 25
+    //val employeeSet : EmployeeSet
+}
