0d732f2c95d1b12a3a6c90ad3e790b6871cc01f6
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-04-02 19:22:11 +0530

    smartcasting in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 import declarations.Employee
+import kotlin.reflect.typeOf
 
 fun main(args: Array<String>) {
    val e1 = Employee("jordiee", "sde2", 23)
    val e2 = Employee("jordiee", "sde2", 23)
-   println(e1 == e2)
-   println(e1 === e2)
-   //structural equaliy (yes)
-
+   val e3 = e1
+   println(e1 != e2)
+   println(e1 != e3)
+   println(e1 !== e2)
+   var x = 34
+   var y = x.toDouble()
+   println(x.javaClass.name)
+   println(y.javaClass.name)
 }
